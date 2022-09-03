32134d21e81f533dc79a35020d3c5a2814178468
Alessandra Morales <azmoralesm@pucp.edu.pe>
2022-09-03 18:10:11 -0500

    Tres en Raya terminado ahora sí :v y un poco de memoria interfaces

--- app/src/main/java/com/example/applab1/EstadisticsTresActivity.java
+++ app/src/main/java/com/example/applab1/EstadisticsTresActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -23,9 +25,18 @@ public class EstadisticsTresActivity extends AppCompatActivity {
         LinearLayout layout = findViewById(R.id.layoutEst);
         for(int i=0; i< est.size(); i++){
             TextView tvEst = new TextView(this);
+            tvEst.setTextSize(18);
             tvEst.setText("Juego "+ (i+1) + ": "+ est.get(i));
             layout.addView(tvEst);
         }
+
+        Button btnNuevo= findViewById(R.id.btn_nuevo_est_tres);
+        btnNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               finish();
+            }
+        });
 
     }
 }
