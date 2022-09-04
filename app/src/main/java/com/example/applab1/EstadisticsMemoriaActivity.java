596ef4e7e6e732d2cbe412ae893191ab194b91b6
Alessandra Morales <azmoralesm@pucp.edu.pe>
2022-09-04 00:46:17 -0500

    Estadísticas pero falta el tiempo

--- app/src/main/java/com/example/applab1/EstadisticsMemoriaActivity.java
+++ app/src/main/java/com/example/applab1/EstadisticsMemoriaActivity.java
@@ -2,7 +2,14 @@ package com.example.applab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class EstadisticsMemoriaActivity extends AppCompatActivity {
 
@@ -11,5 +18,23 @@ public class EstadisticsMemoriaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadistics_memoria);
         setTitle("Juego - Memoria");
+
+        Intent intent = this.getIntent();
+        ArrayList<String> est = intent.getStringArrayListExtra("estadisticasMem");
+        LinearLayout layout = findViewById(R.id.layoutMemoria);
+        for(int i=0; i< est.size(); i++){
+            TextView tvEst = new TextView(this);
+            tvEst.setTextSize(18);
+            tvEst.setText("Juego "+ (i+1) + ": "+ est.get(i));
+            layout.addView(tvEst);
+        }
+
+        Button btnNuevo= findViewById(R.id.btn_nuevo_est);
+        btnNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/applab1/MainActivity2.java
+++ app/src/main/java/com/example/applab1/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.applab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,6 +24,7 @@ public class MainActivity2 extends AppCompatActivity {
     private ArrayList<String> estadisticas = new ArrayList<>();
     private ArrayList<Button> botonesBloqueados = new ArrayList<>();
     private ArrayList<Button> botonesElegidos = new ArrayList<>();
+    private String estado = "jugando";
     private Instant inicio;
 
     @Override
@@ -31,55 +33,7 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         setTitle("Inicio");
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            inicio = Instant.now();
-        }
-
-        letras.add("A");
-        letras.add("A");
-        letras.add("B");
-        letras.add("B");
-        letras.add("C");
-        letras.add("C");
-        letras.add("D");
-        letras.add("D");
-        letras.add("E");
-        letras.add("E");
-        letras.add("F");
-        letras.add("F");
-        letras.add("G");
-        letras.add("G");
-        letras.add("H");
-        letras.add("H");
-
-        Collections.shuffle(letras);
-
-        //Introducir en una función :'v
-        ((Button) findViewById(R.id.btn11m)).setText(letras.get(0));
-        ((Button) findViewById(R.id.btn12m)).setText(letras.get(1));
-        ((Button) findViewById(R.id.btn13m)).setText(letras.get(2));
-        ((Button) findViewById(R.id.btn14m)).setText(letras.get(3));
-        ((Button) findViewById(R.id.btn21m)).setText(letras.get(4));
-        ((Button) findViewById(R.id.btn22m)).setText(letras.get(5));
-        ((Button) findViewById(R.id.btn23m)).setText(letras.get(6));
-        ((Button) findViewById(R.id.btn24m)).setText(letras.get(7));
-        ((Button) findViewById(R.id.btn31m)).setText(letras.get(8));
-        ((Button) findViewById(R.id.btn32m)).setText(letras.get(9));
-        ((Button) findViewById(R.id.btn33m)).setText(letras.get(10));
-        ((Button) findViewById(R.id.btn34m)).setText(letras.get(11));
-        ((Button) findViewById(R.id.btn41m)).setText(letras.get(12));
-        ((Button) findViewById(R.id.btn42m)).setText(letras.get(13));
-        ((Button) findViewById(R.id.btn43m)).setText(letras.get(14));
-        ((Button) findViewById(R.id.btn44m)).setText(letras.get(15));
-
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                metodoaEjecutarPasadoDossegundos();
-            }
-        }, 2000);
+        inicioJuego();
 
     }
 
@@ -161,30 +115,105 @@ public class MainActivity2 extends AppCompatActivity {
         assert view instanceof Button;
         Button btnClick = (Button) view;
         Log.d("lol", String.valueOf(botonesElegidos.size()));
-        if(botonesElegidos.size()<2){
-            mostrarBtn(btnClick);
-            botonesElegidos.add(btnClick);
-            if(botonesElegidos.size()==2){
-                Button buttonElegido= botonesElegidos.get(0);
-                Button buttonElegido2 = botonesElegidos.get(1);
-                if(!botonesElegidos.get(0).getText().toString().equals(botonesElegidos.get(1).getText().toString())){
-                    Log.d("lol","son diferentes");
-                    Handler handler1 = new Handler();
-                    handler1.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            buttonElegido.setText("-");
-                            buttonElegido2.setText("-");
-                        }
-                    }, 500);
-                }else{
-                    Log.d("lol","son iguales");
-                    mostrarBtn(buttonElegido);
-                    mostrarBtn(buttonElegido2);
+        if(botonesBloqueados.size()!=16){
+            if(botonesElegidos.size()<2){
+                mostrarBtn(btnClick);
+                botonesElegidos.add(btnClick);
+                if(botonesElegidos.size()==2){
+                    Button buttonElegido= botonesElegidos.get(0);
+                    Button buttonElegido2 = botonesElegidos.get(1);
+                    if(!botonesElegidos.get(0).getText().toString().equals(botonesElegidos.get(1).getText().toString())){
+                        Log.d("lol","son diferentes");
+                        Handler handler1 = new Handler();
+                        handler1.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                buttonElegido.setText("-");
+                                buttonElegido2.setText("-");
+                            }
+                        }, 500);
+                    }else{
+                        Log.d("lol","son iguales");
+                        mostrarBtn(buttonElegido);
+                        mostrarBtn(buttonElegido2);
+                        botonesBloqueados.add(buttonElegido);
+                        botonesBloqueados.add(buttonElegido2);
+                    }
+                    botonesElegidos.clear();
                 }
-                botonesElegidos.clear();
             }
+        }else{
+            estado = "gano";
+            estadisticas.add("Ganó");
+            Log.d("lol","gano");
         }
+    }
+
+    public void inicioJuego(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            inicio = Instant.now();
+        }
+
+        letras.add("A");
+        letras.add("A");
+        letras.add("B");
+        letras.add("B");
+        letras.add("C");
+        letras.add("C");
+        letras.add("D");
+        letras.add("D");
+        letras.add("E");
+        letras.add("E");
+        letras.add("F");
+        letras.add("F");
+        letras.add("G");
+        letras.add("G");
+        letras.add("H");
+        letras.add("H");
+
+        Collections.shuffle(letras);
+
+        //Introducir en una función :'v
+        ((Button) findViewById(R.id.btn11m)).setText(letras.get(0));
+        ((Button) findViewById(R.id.btn12m)).setText(letras.get(1));
+        ((Button) findViewById(R.id.btn13m)).setText(letras.get(2));
+        ((Button) findViewById(R.id.btn14m)).setText(letras.get(3));
+        ((Button) findViewById(R.id.btn21m)).setText(letras.get(4));
+        ((Button) findViewById(R.id.btn22m)).setText(letras.get(5));
+        ((Button) findViewById(R.id.btn23m)).setText(letras.get(6));
+        ((Button) findViewById(R.id.btn24m)).setText(letras.get(7));
+        ((Button) findViewById(R.id.btn31m)).setText(letras.get(8));
+        ((Button) findViewById(R.id.btn32m)).setText(letras.get(9));
+        ((Button) findViewById(R.id.btn33m)).setText(letras.get(10));
+        ((Button) findViewById(R.id.btn34m)).setText(letras.get(11));
+        ((Button) findViewById(R.id.btn41m)).setText(letras.get(12));
+        ((Button) findViewById(R.id.btn42m)).setText(letras.get(13));
+        ((Button) findViewById(R.id.btn43m)).setText(letras.get(14));
+        ((Button) findViewById(R.id.btn44m)).setText(letras.get(15));
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                metodoaEjecutarPasadoDossegundos();
+            }
+        }, 2000);
+
+    }
+
+    public void nuevoJuego(View view){
+        if(botonesBloqueados.size()!=16 && estado.equals("jugando")){
+            estadisticas.add("Canceló");
+        }
+        inicioJuego();
+    }
+
+    public void estadisticas(View view){
+
+        Intent intent= new Intent(MainActivity2.this,EstadisticsMemoriaActivity.class);
+        intent.putExtra("estadisticasMem",estadisticas);
+        startActivity(intent);
 
     }
 
